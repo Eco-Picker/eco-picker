f2bf6e809ea57361f1aa3d14e947c5a60d12aa1e
Minji Kim <minjikm19@gmail.com>
2024-07-23 17:14:53 -0400

    Added location permission logic

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,7 +1,7 @@
+import 'package:eco_picker/utils/geolocator_utils.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import '../main.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class MapPage extends StatefulWidget {
   @override
@@ -10,33 +10,86 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   late GoogleMapController mapController;
+  LatLng? _currentPosition;
+  bool _isLoading = true;
+  final GeolocatorUtil _geolocatorUtil = GeolocatorUtil();
+  Set<Marker>? _markers = <Marker>{};
+  late BitmapDescriptor myMarker;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  void setMarkerIcon() async {
+    myMarker = await BitmapDescriptor.fromAssetImage(
+        const ImageConfiguration(size: Size(50, 50)), 'loginlogo.png');
+  }
+
+  Future<void> getLocation() async {
+    var status = await Permission.locationWhenInUse.status;
+    if (!status.isGranted) {
+      await Permission.locationWhenInUse.request();
+    }
+
+    final position = await _geolocatorUtil.getCurrentLocation();
+    if (position != null) {
+      setState(() {
+        _currentPosition = LatLng(position.latitude, position.longitude);
+        _isLoading = false;
+      });
+    }
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+    // generateMarkers();
+  }
+
+  void generateMarkers() {
+    var localMarkers = <Marker>{};
+
+    // for (var location in locationsList!) {
+    //   localMarkers.add(Marker(
+    //       markerId: MarkerId(location.id!),
+    //       position: LatLng(location.lat!, location.lng!),
+    //       icon: myMarker));
+    // }
+
+    if (mounted) {
+      setState(() {
+        _markers = localMarkers;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.green[700],
+    // setMarkerIcon();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Map'),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Maps Sample App'),
-          elevation: 2,
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-        ),
-      ),
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _currentPosition!,
+                zoom: 16.0,
+              ),
+              myLocationEnabled: true, // Show the user's current location
+              markers: {
+                const Marker(
+                  markerId: const MarkerId("Sydney"),
+                  position: LatLng(-33.86, 151.20),
+                )
+              },
+              // markers: _markers!,
+            ),
     );
   }
 }
