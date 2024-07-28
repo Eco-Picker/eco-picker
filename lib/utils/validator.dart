fc3d5ea087f029f42a54378982994dc0bc01cc83
Minji Kim <minjikm19@gmail.com>
2024-07-28 00:47:18 -0400

    Added password validation

--- lib/utils/validator.dart
+++ lib/utils/validator.dart
@@ -9,3 +9,29 @@ String? validateEmail(String? value) {
   }
   return null;
 }
+
+String? validatePassword(String? value) {
+  if (value == null || value.isEmpty) {
+    return 'Please enter your password';
+  }
+  if (value.length < 8) {
+    return 'Password must be at least 8 characters long';
+  }
+
+  bool hasUpperCase = value.contains(RegExp(r'[A-Z]'));
+  bool hasLowerCase = value.contains(RegExp(r'[a-z]'));
+  bool hasDigit = value.contains(RegExp(r'[0-9]'));
+  bool hasSpecialCharacter = value.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'));
+
+  int count = 0;
+  if (hasUpperCase) count++;
+  if (hasLowerCase) count++;
+  if (hasDigit) count++;
+  if (hasSpecialCharacter) count++;
+
+  if (count < 3) {
+    return 'Password must contain at least 3 of the following: \nuppercase letter, lowercase letter, \nnumber, special character';
+  }
+
+  return null;
+}
